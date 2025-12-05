b67cb78aab6050994765ce956f986c9c755d8b22
Omansh Choudhary <omansh.choudhary@gmail.com>
2025-12-05 23:35:51 +0530

    Add input area

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
+import { useState } from 'react'
 import './App.css'
 import CodeEditor from './components/CodeEditor'
 
 function App() {
+  const [language, setLanguage] = useState("54");
+  const [code, setCode] = useState("Write your code here");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
   return (
     <>
-      <div className="bg-black text-white h-[70px]">
-        <h1 className="text-3xl font-semibold">Clutch</h1>
-        <p className="text-sm text-gray-300 mt-1">Powered by Monaco + Tailwind</p>
+      <div className="bg-black text-white h-[65px] flex flex-row justify-between items-center ">
+        <div className="text-3xl font-semibold mr-2">Clutch</div>
+        <select className="bg-gray-800 text-white px-3 py-2 rounded" value={language}
+          onChange={(e) => setLanguage(e.target.value)}>
+          <option value="54">C++</option>
+          <option value="62">Java</option>
+          <option value="71">Python</option>
+          <option value="50">C</option>
+          <option value="63">JavaScript</option>
+        </select>
       </div>
-      <CodeEditor />
+      <main className="h-[calc(100vh-70px)] flex flex-col gap-3 bg-black text-white px-4 pb-4">
+        <div className="flex-1 min-h-0 border border-gray-800 rounded-lg overflow-hidden">
+          <CodeEditor code={code} setCode={setCode} language={language} />
+        </div>
+        <textarea
+          className="w-full bg-gray-900 text-white h-32 resize-none rounded-lg p-3 border border-gray-800"
+          rows={4}
+          placeholder="Input"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+      </main>
     </>
-
   )
 }
 
--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react"
 import Editor from "@monaco-editor/react";
 
-export default function CodeEditor() {
-    const [code, setCode] = useState("Write your code here")
-    return (
-        <main className="h-[calc(100vh-70px)]">
-            <Editor
-                height="100%"
-                language="cpp"
-                theme="vs-dark"
-                value={code}
-                onChange={value => setCode(value)}
-            />
-        </main>
-    )
+export default function CodeEditor(props) {
+  return (
+    <Editor
+      height="100%"
+      language={props.language ? languageMap[props.language] || "cpp" : "cpp"}
+      theme="vs-dark"
+      value={props.code}
+      onChange={value => props.setCode(value)}
+    />
+  )
+}
+
+
+const languageMap = {
+  54: "cpp",
+  62: "java",
+  71: "python",
+  50: "c",
+  63: "javascript",
 }
 
